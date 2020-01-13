5331091ffb2ca1f5754dbd841fa9f1a51bdee10b
Azahara Martinez <azaharamaga@gmail.com>
2020-01-13 13:25:48 +0100

    Add characterDetail styles

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,18 +1,33 @@
 import React from "react";
+import "../stylesheet/CharacterDetail.scss";
 
 function CharacterDetail(props) {
   const { image, name, species, location, episode, status } = props.selectedCharacter;
   return (
-    <section>
-      <img src={image} alt={`Character ${name}`} />
-      <h2>{name}</h2>
-      <ul>
-        <li>Status: {status}</li>
-        <li>Species: {species}</li>
-        <li>Origin: {location.name}</li>
-        <li>Episodes: {episode.length}</li>
-      </ul>
-    </section>
+    <main className="main">
+      <section className="section">
+        <img className="section__img" src={image} alt={`Character ${name}`} />
+        <div className="section__container">
+          <h2 className="section__title">{name}</h2>
+          <ul className="section__list">
+            <li className="section__list--item">
+              <strong>Status:</strong> {status}
+            </li>
+            <li className="section__list--item">
+              <strong>Species:</strong> {species}
+            </li>
+            <li className="section__list--item">
+              <strong>Origin:</strong>
+              {location.name}
+            </li>
+            <li className="section__list--item">
+              <strong>Episodes:</strong>
+              {episode.length}
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
   );
 }
 
