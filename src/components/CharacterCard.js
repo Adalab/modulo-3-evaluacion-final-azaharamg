046617798cc5740b030c250d9f2e4801b2840f37
Azahara Martinez <azaharamaga@gmail.com>
2020-01-13 18:13:10 +0100

    add species icon

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,13 +3,27 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "../stylesheet/CharacterCard.scss";
 
+const speciesIcon = species => {
+  if (species === "Humanoid") {
+    return "fas fa-robot";
+  } else if (species === "Alien") {
+    return "fab fa-reddit-alien";
+  } else if (species === "Animal") {
+    return "fas fa-paw";
+  } else if (species === "Human") {
+    return "fas fa-male";
+  }
+};
+
 function CharacterCard(props) {
   const { id, image, name, species } = props.characterItem;
   return (
     <Link className="list__item--link" to={`/character/${id}`}>
       <img className="list__item--img" src={image} alt={`character ${name}`} />
       <h3 className="list__item--title">{name}</h3>
-      <p className="list__item--paragraph">{species}</p>
+      <i className={`list__item--paragraph ${speciesIcon(species)}`}>
+        {species === "Vampire" ? `It is a ${species} !!!` : species}
+      </i>
     </Link>
   );
 }
