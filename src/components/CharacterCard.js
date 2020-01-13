11d3e94838fe254f70a60121157ebb47fe1752c9
Azahara Martinez <azaharamaga@gmail.com>
2020-01-13 12:48:27 +0100

    Add characterCard styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../stylesheet/CharacterCard.scss";
 
 function CharacterCard(props) {
   const { id, image, name, species } = props.characterItem;
   return (
-    <Link to={`/character/${id}`}>
-      <img src={image} alt={`character ${name}`} />
-      <h2>{name}</h2>
-      <p>{species}</p>
+    <Link className="list__item--link" to={`/character/${id}`}>
+      <img className="list__item--img" src={image} alt={`character ${name}`} />
+      <h3 className="list__item--title">{name}</h3>
+      <p className="list__item--paragraph">{species}</p>
     </Link>
   );
 }
