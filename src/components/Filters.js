59b2049fac75bbb40af89cf0c23c4a03c6e244ea
Azahara Martinez <azaharamaga@gmail.com>
2020-01-14 12:40:03 +0100

    Prevent event default

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,13 +3,16 @@ import "../stylesheet/Filters.scss";
 import PropTypes from "prop-types";
 
 function Filters(props) {
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
   const handleSearch = event => {
     const searchText = event.target.value.toLowerCase();
     props.handleSearch(searchText);
   };
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <input
         className="form__searchBar"
         type="text"
